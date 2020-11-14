0b42af956ffb3a644078d1ef9447b92581719349
KathaVill <64733736+KathaVill@users.noreply.github.com>
2020-11-14 16:33:54 +1100

    playback buttons

--- Assets/_2_Scripts/MediaController.cs
+++ Assets/_2_Scripts/MediaController.cs
@@ -25,6 +25,7 @@ public class MediaController : MonoBehaviour
     private FirstPersonController movement;
     private bool hasPlayed = false;
     private GameObject player;
+    private long pauseFrame;
     
 
     // Start is called before the first frame update
@@ -59,9 +60,9 @@ public class MediaController : MonoBehaviour
 
 
         }
-        if (vidPlyr != null)
+        if (vidPlyr != null && isStreaming)
         {
-            if (isStreaming && Input.GetKey("e"))
+            if (Input.GetKey("e"))
             {
                 if (spawnPoint != null)
                 {
@@ -73,9 +74,25 @@ public class MediaController : MonoBehaviour
 
                 StopVideo();
             }
+
             long playerCurrentFrame = vidPlyr.GetComponent<VideoPlayer>().frame;
             long playerFrameCount = Convert.ToInt64(vidPlyr.GetComponent<VideoPlayer>().frameCount);
-            if (isStreaming && playerCurrentFrame ==playerFrameCount)
+            if (Input.GetKeyDown("space") && playerCurrentFrame>pauseFrame)
+            {
+                print("pausing");
+                vidPlyr.Pause();
+                pauseFrame = playerCurrentFrame;
+            }
+            else if (Input.GetKeyDown("space") && playerCurrentFrame == pauseFrame)
+            {
+                print("playing");
+                vidPlyr.Play();
+            }
+            else if (Input.GetKeyDown("r"))
+            {
+                RestartVideo();
+            }
+            if (playerCurrentFrame ==playerFrameCount)
             {
                 if (spawnPoint != null)
                 {
@@ -83,18 +100,34 @@ public class MediaController : MonoBehaviour
                     controller.enabled = false;
                     player.transform.position = spawnPoint.transform.position;
                     controller.enabled = true;
+                    player.transform.Rotate(0.0f, -90.0f, 0.0f, Space.World);
                 }
                 
                 StopVideo();
             }
-            hasPlayed = true;
+            
         }
 
     }
 
+    void RestartVideo()
+    {
+        vidPlyr.loopPointReached += EndReached;
+        vidPlyr.Stop();
+        vidPlyr.Prepare();
+        vidPlyr.loopPointReached += EndReached;
+        pauseFrame = 0;
+        vidPlyr.frame = 0;
+        vidPlyr.Play();
+    }
+
+
     void StopVideo()
     {
+
+        vidPlyr.loopPointReached += EndReached;
         vidPlyr.Stop();
+
         //vidPlyr.enabled = false;
         Task fader_t = new Task(FadeInRawImage(fadeSpeed, img, false));
 
@@ -102,6 +135,7 @@ public class MediaController : MonoBehaviour
 
         Task fader2_t = new Task(FadeToBlack(fadeSpeed, fadeDelay + 1 / fadeAwaySpeed, false));
         isStreaming = false;
+        hasPlayed = true;
     }
     void PrepareVideo()
     {
@@ -110,6 +144,8 @@ public class MediaController : MonoBehaviour
 
         vidPlyr.clip = videoClip;
         vidPlyr.Prepare();
+        vidPlyr.loopPointReached += EndReached;
+        pauseFrame = 0;
     }
     void PlayVideo(GameObject canvas)
     {
@@ -118,15 +154,21 @@ public class MediaController : MonoBehaviour
         vidPlyr.enabled = true;
         vidPlyr.clip = videoClip;
         vidPlyr.Prepare();
-
+        vidPlyr.frame = 0;
         vidPlyr.Play();
         img = canvas.GetComponentInChildren<RawImage>();
+        //img = new RawImage();
         img.enabled = true;
+       // img.texture = 
         
         var fader_t = new Task( FadeInRawImage(fadeAwaySpeed, img));
         //Cursor.visible = true;
     }
-
+    void EndReached(VideoPlayer vp)
+    {
+        // Reset video to first frame
+        vp.frame = 0;
+    }
     //https://turbofuture.com/graphic-design-video/How-to-Fade-to-Black-in-Unity#:~:text=Method%201%3A%20Attach%20To%20Camera&text=In%20Unity%2C%20go%20to%20Assets,Voila.
     void OnTriggerEnter(Collider other)
     {
@@ -188,7 +230,7 @@ public class MediaController : MonoBehaviour
     {
         Color objColor = img.color;
         float fadeAmount;
-        //yield return new WaitForSeconds(fadeDelay);
+        yield return new WaitForSeconds(fadeDelay*0.3f);
         if (fade)
         {
             while (objColor.a < 1)
